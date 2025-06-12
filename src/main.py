20d75a445b057b2326fc7e7701dabbf82e4445af
dudispires <edumcpires@aluno.ufsj.edu.br>
2025-06-12 18:21:30 -0300

    :sparkles: New main :

---
+++ src/main.py
@@ -0,0 +1,23 @@
+from simplex_code2 import Simplex
+import numpy as np
+
+def main():
+    A = np.array([[2, 1],
+                  [1, 2]])
+    b = np.array([20, 20])
+    z = np.array([10, 12])  # Maximizar Z = 10 x1 + 12 x2
+
+    simplex = Simplex(A, b, z)
+    simplex.cria_tabela()
+    #simplex.exibe_solucao()
+    #simplex.quem_entra_quem_sai()
+    #simplex.cria_nova_tabela()
+
+    with open("src/entrada.txt", "r", encoding="utf-8") as arquivo:
+        linhas = arquivo.readlines()
+        
+        linhas= linhas.split(",")
+        print(linhas)
+        #Sprint(linhas)
+if __name__ == "__main__":
+    main()
