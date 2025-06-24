ec2f9322cad000440c6dfe5e992ac51c03d0698d
dudispires <edumcpires@aluno.ufsj.edu.br>
2025-06-24 12:59:39 -0300

    :memo: Nova main para receber arquivo de entrada e utilizar o SIMPLEX duas fases

--- src/main.py
+++ src/main.py
@@ -1,21 +1,24 @@
-#from simplex_code2 import Simplex
-from teste import Simplex
 import numpy as np
+import json
+from simplex_duas_fases import TwoPhaseSimplex
+
+def carregar_problema_json(caminho_arquivo):
+    with open(caminho_arquivo, 'r') as f:
+        dados = json.load(f)
+
+    coeficientes = np.array(dados['coeficientes'], dtype=float)
+    independentes = np.array(dados['independentes'], dtype=float)
+    f_obj = np.array(dados['f_obj'], dtype=float)
+    operadores = np.array(dados['operadores'])
+
+    return coeficientes, independentes, f_obj, operadores
 
 def main():
-    A = np.array([[2, 1],
-                  [1, 2]])
-    operadores= np.array((['<=','>=']))
-    b = np.array([20, 20])
-    z = np.array([10, 12])  # Maximizar Z = 10 x1 + 12 x2
+    caminho = 'src/problema.json'
+    coef, indep, fobj, ops = carregar_problema_json(caminho)
 
-    simplex = Simplex(A, b, z, operadores)
-    #simplex.m_grande()
-    simplex.cria_tabela()
-    #simplex.exibe_solucao()
-    #simplex.quem_entra_quem_sai()
-    #simplex.cria_nova_tabela()
-
+    solver = TwoPhaseSimplex(coef, indep, fobj, ops)
+    solver.resolver()
 
 if __name__ == "__main__":
     main()
